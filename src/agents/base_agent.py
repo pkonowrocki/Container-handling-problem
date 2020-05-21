64b1298fe99253251575824b5f1057defdb31fe5
Andżelika Domańska <andzelika.domanska@prudential.pl>
2020-05-21 23:31:59 +0200

    Add BaseAgent and port terminal ontology basic class

---
+++ src/agents/base_agent.py
@@ -0,0 +1,21 @@
+from typing import Sequence
+
+from spade.agent import Agent
+
+from src.ontology.content_manager import ContentManager
+from src.ontology.ontology import Ontology
+
+
+class BaseAgent(Agent):
+    def __init__(self, jid: str, password: str, ontologies: Sequence[Ontology]):
+        super().__init__(jid, password)
+        self._content_manager = ContentManager()
+        for onto in ontologies:
+            self._content_manager.register_ontology(onto)
+
+    @property
+    def content_manager(self) -> ContentManager:
+        return self._content_manager
+
+    def log(self, text: str):
+        print(f'{self.name}: {text}')
